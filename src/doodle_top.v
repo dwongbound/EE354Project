7d2371da96ea2c6e5ffb3f9eaecd524809c1a23e
Julie Deng <julieden@usc.edu>
2023-04-13 19:30:10 -0700

    Finished top design

--- src/doodle_top.v
+++ src/doodle_top.v
@@ -48,9 +48,9 @@ module vga_top(
 	vga_bitchange vbc(.clk(ClkPort), .bright(bright), .button(BtnU), .hCount(hc), .vCount(vc), .rgb(rgb), .score(score));
 	counter cnt(.clk(ClkPort), .displayNumber(score), .anode(anode), .ssdOut(ssdOut));
 	
-	assign Dp = 1;
-	assign {Ca, Cb, Cc, Cd, Ce, Cf, Cg} = ssdOut[6 : 0];
-    assign {An7, An6, An5, An4, An3, An2, An1, An0} = {4'b1111, anode};
+	//assign Dp = 1;
+	//assign {Ca, Cb, Cc, Cd, Ce, Cf, Cg} = ssdOut[6 : 0];
+    //assign {An7, An6, An5, An4, An3, An2, An1, An0} = {4'b1111, anode};
 
 	
 	assign vgaR = rgb[11 : 8];
@@ -66,21 +66,19 @@ module vga_top(
 	wire [7:0] J, M, Curr, i_score;
 	wire [1:0] ssdscan_clk;
 	reg [7:0] Jin, Min;
+	reg [3:0]	SSD;
+	wire [3:0]	SSD3, SSD2, SSD1, SSD0;
+	reg [7:0]  SSD_CATHODES;
+
+	// disable mamory ports
+	assign {MemOE, MemWR, RamCS, QuadSpiFlashCS} = 4'b1111;
 
 	assign board_clk = ClkPort;
 
-	assign ssdscan_clk = DIV_CLK[19:18];
-
-	assign AN0	= ~(~(ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 00
-	assign AN1	= ~(~(ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 01
-	assign AN2	= ~( (ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 10
-	assign AN3	= ~( (ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 11
-
-	assign {AN7, AN6, AN5, AN4} = 4'b1111;
-	/* Hard code Jin and Min */
-	
 	assign Reset = BtnC;
-	assign Start_Ack_Pulse = BtnL;
+	assign Start_Ack_Pulse = BtnU;
+
+	/* Hard code Jin and Min */
 
 	always @(posedge board_clk, posedge reset) 	
     begin							
@@ -97,15 +95,58 @@ module vga_top(
 	doodle_core doodle_sm(.Clk(sys_clk), .Reset(Reset), .Start(Start_Ack_Pulse), .Ack(Start_Ack_Pulse), .Jin(Jin), .J(J), 
 						  .Min(Min), .M(M), .Curr(Curr), .i_score(i_score), .q_I(q_I), .q_Up(q_Up), .q_Down(q_Down), .q_Done(q_Done));
 
-	/* Use SSDs to print score when arriving in DONE state */
-
 
 	/* Use LEDs to see which state we're in */
 	assign {Ld7, Ld6, Ld5, Ld4} = {q_I, q_Up, q_Down, q_Done};
 
+	assign SSD3 = 4'b0000;
+	assign SSD2 = 4'b0000;
+	assign SSD1 = (q_Done) ? i_score[7:4] : 4'b0000;
+	assign SSD0 = (q_DONE) ? i_score[3:0] : 4'b0000;
 
+	assign ssdscan_clk = DIV_CLK[19:18];
 
-	// disable mamory ports
-	assign {MemOE, MemWR, RamCS, QuadSpiFlashCS} = 4'b1111;
+	assign AN0	= ~(~(ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 00
+	assign AN1	= ~(~(ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 01
+	assign AN2	= ~( (ssdscan_clk[1]) && ~(ssdscan_clk[0]));  // when ssdscan_clk = 10
+	assign AN3	= ~( (ssdscan_clk[1]) &&  (ssdscan_clk[0]));  // when ssdscan_clk = 11
+
+	assign {AN7, AN6, AN5, AN4} = 4'b1111;
+
+	always @ (ssdscan_clk, SSD0, SSD1, SSD2, SSD3)
+	begin : SSD_SCAN_OUT
+		case (ssdscan_clk) 
+				  2'b00: SSD = SSD3     ;	// ****** TODO  in Part 2 ******
+				  2'b01: SSD = SSD2    ;  	// Complete the four lines
+				  
+				  2'b10: SSD = SSD1   ;
+				  2'b11: SSD = SSD0    ;
+		endcase 
+	end
+
+	/* Use SSDs to print score when arriving in DONE state */
+	assign {Ca, Cb, Cc, Cd, Ce, Cf, Cg, Dp} = {SSD_CATHODES};
+	always @ (SSD) 
+	begin : HEX_TO_SSD
+		case (SSD) 
+			4'b0000: SSD_CATHODES = 8'b00000011; // 0
+			4'b0001: SSD_CATHODES = 8'b10011111; // 1
+			4'b0010: SSD_CATHODES = 8'b00100101; // 2
+			4'b0011: SSD_CATHODES = 8'b00001101; // 3
+			4'b0100: SSD_CATHODES = 8'b10011001; // 4
+			4'b0101: SSD_CATHODES = 8'b01001001; // 5
+			4'b0110: SSD_CATHODES = 8'b01000001; // 6
+			4'b0111: SSD_CATHODES = 8'b00011111; // 7
+			4'b1000: SSD_CATHODES = 8'b00000001; // 8
+			4'b1001: SSD_CATHODES = 8'b00001001; // 9
+			4'b1010: SSD_CATHODES = 8'b00010001; // A
+			4'b1011: SSD_CATHODES = 8'b11000001; // B
+			4'b1100: SSD_CATHODES = 8'b01100011; // C
+			4'b1101: SSD_CATHODES = 8'b10000101; // D
+			4'b1110: SSD_CATHODES = 8'b01100001; // E
+			4'b1111: SSD_CATHODES = 8'b01110001; // F    
+			default: SSD_CATHODES = 8'bXXXXXXXX; // default is not needed as we covered all cases
+		endcase
+	end	
 
 endmodule
